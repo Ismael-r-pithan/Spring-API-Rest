96d6aafa81e528113faf145cd4df589682b0f2ff
Ismael-r-pithan <pithanismael@gmail.com>
2022-02-04 22:20:55 -0300

    adicionando post e incluindo melhorias controller

--- src/main/java/com/meuprojeto/meuapp/controller/CategoriaController.java
+++ src/main/java/com/meuprojeto/meuapp/controller/CategoriaController.java
@@ -1,14 +1,22 @@
 package com.meuprojeto.meuapp.controller;
 
+import java.net.URI;
 import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
 
 import com.meuprojeto.meuapp.model.Categoria;
 import com.meuprojeto.meuapp.repository.CategoriaRepository;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import lombok.AllArgsConstructor;
 
@@ -20,9 +28,22 @@ public class CategoriaController {
     private CategoriaRepository categoriaRepository;
     
     @GetMapping
-    public ResponseEntity<?> buscarTodas() {
+    public ResponseEntity<?> buscarTodas () {
         List<Categoria> categorias = categoriaRepository.findAll();
 
         return !categorias.isEmpty() ? ResponseEntity.ok(categorias) : ResponseEntity.noContent().build();
     }
+
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void adicionar (@RequestBody Categoria categoria, HttpServletResponse response) {
+        Categoria categoriaAdicionada = categoriaRepository.save(categoria);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}")
+            .buildAndExpand(categoriaAdicionada.getId()).toUri();
+
+        response.setHeader("Location", uri.toASCIIString());
+    }
+
 }
