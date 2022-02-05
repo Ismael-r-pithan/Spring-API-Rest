9820a5a4bdea9444d37377da7e97474dda713fd2
Ismael-r-pithan <pithanismael@gmail.com>
2022-02-04 23:23:26 -0300

    melhorando respostas http e 'validando' entrada de dados/propriedades json

--- src/main/java/com/meuprojeto/meuapp/controller/CategoriaController.java
+++ src/main/java/com/meuprojeto/meuapp/controller/CategoriaController.java
@@ -2,6 +2,7 @@ package com.meuprojeto.meuapp.controller;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 import javax.servlet.http.HttpServletResponse;
 
@@ -11,6 +12,7 @@ import com.meuprojeto.meuapp.repository.CategoriaRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,6 +36,17 @@ public class CategoriaController {
         return !categorias.isEmpty() ? ResponseEntity.ok(categorias) : ResponseEntity.noContent().build();
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<?> buscarPorId (@PathVariable Long id) {
+        Optional<Categoria> categoria = categoriaRepository.findById(id);
+
+        if (categoria.isPresent()) {
+            return ResponseEntity.ok(categoria);
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
@@ -45,5 +58,8 @@ public class CategoriaController {
 
         response.setHeader("Location", uri.toASCIIString());
     }
+
+
+    
 
 }
